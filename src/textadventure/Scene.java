4b7c7eb9ea24cd85414b178dd9fb232ede4f670c
jpinaz92 <jpinaz92@gmail.com>
2016-10-05 21:29:40 +1100

    added getID to scene
    added gerID function to scene

--- src/textadventure/Scene.java
+++ src/textadventure/Scene.java
@@ -59,6 +59,10 @@ public class Scene {
     public String getId() {
         return id;
     }
+    
+    public void setId(String id) {
+        this.id = id;
+    }
 
     public String getDescription() {
         return description;
